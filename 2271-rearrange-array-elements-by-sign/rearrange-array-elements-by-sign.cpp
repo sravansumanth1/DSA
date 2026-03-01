29296fe58ee3da6806c243a72e27a83baa508571
sravansumanth1 <sravansumanth141@gmail.com>
2026-03-02 01:06:36 +0530

    Time: 18 ms (5.11%) | Memory: 139 MB (23.99%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -7,12 +7,17 @@ public:
             if(nums[i]>0)pos.push_back(nums[i]);
             else neg.push_back(nums[i]);
         }
-        vector<int>ans;
-        int pp=0;
-        int np=0;
-        for(int i=0;i<nums.size();i++){
-            if(i%2==0)ans.push_back(pos[pp++]);
-            else ans.push_back(neg[np++]);
+        // vector<int>ans;
+        // int pp=0;
+        // int np=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(i%2==0)ans.push_back(pos[pp++]);
+        //     else ans.push_back(neg[np++]);
+        // }
+        vector<int>ans(nums.size());
+        for(int i=0;i<(nums.size())/2;i++){
+            ans[2*i]=pos[i];
+            ans[2*i+1]=neg[i];
         }
         return ans;
     }
